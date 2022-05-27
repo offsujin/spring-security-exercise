3878688796fcfa740b1e6711fbe9eb6cdc2705a5
offsujin <offsujin@gmail.com>
2022-05-27 22:47:30 +0900

    Add Scan Config

--- src/main/java/com/inflearn/securityex1/SecurityEx1Application.java
+++ src/main/java/com/inflearn/securityex1/SecurityEx1Application.java
@@ -2,8 +2,12 @@ package com.inflearn.securityex1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = {"com.inflearn.securityex1.repository"})
+@EntityScan(basePackages = {"com.inflearn.securityex1"})
 public class SecurityEx1Application {
 
 	public static void main(String[] args) {
